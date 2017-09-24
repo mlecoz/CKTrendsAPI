9d3480daf9e628009a1472cc8e96fdc1b0e65a49
Marissa Le Coz <Marissa.C.Le.Coz.17@dartmouth.edu>
2017-09-23 23:04:41 -0400

    add xcode project

---
+++ SampleDeveloperApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SampleDeveloperApp
+//
+//  Created by Marissa Le Coz on 9/23/17.
+//  Copyright © 2017 Marissa Le Coz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
